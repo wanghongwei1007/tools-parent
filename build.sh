160cc52fb21f91950119c55e814a9146194ad98b
minikiller@qq.com <minikiller@qq.com>
2018-05-24 07:53:02 +0800

    add exit code

--- build.sh
+++ build.sh
@@ -1,32 +1,20 @@
-cd kalix-parent && git pull origin master && mvn clean install
+cd kalix-parent && git pull origin master && mvn clean install || exit -1
 cd ..
 
-cd framework-parent && git pull origin master && mvn -DskipTests=true clean install
+cd framework-parent && git pull origin master && mvn -DskipTests=true clean install || exit -1
 cd ..
 
-cd admin-parent && git pull origin master && mvn  -DskipTests=true clean install
+cd admin-parent && git pull origin master && mvn  -DskipTests=true clean install || exit -1
 cd ..
 
-cd middleware-parent && git pull origin master && mvn  -DskipTests=true clean install
+cd middleware-parent && git pull origin master && mvn  -DskipTests=true clean install || exit -1
 cd ..
 
-cd oa-parent && git pull origin master && mvn clean install
+cd oa-parent && git pull origin master && mvn clean install || exit -1
 cd ..
 
-cd common-parent && git pull origin master && mvn clean install
+cd common-parent && git pull origin master && mvn clean install || exit -1
 cd ..
 
-cd schedule-parent && git pull origin master && mvn clean install
+cd tools-parent && git pull origin master && mvn clean install || exit -1
 cd ..
-
-cd research-parent && git pull origin master && mvn clean install
-cd ..
-
-cd art-parent && git pull origin master && mvn clean install
-cd ..
-
-cd tools-parent && git pull origin master && mvn clean install
-cd ..
-
-
-
